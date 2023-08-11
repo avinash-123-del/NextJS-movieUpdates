60a0bf9ccac5f750bb12ea7b085cab34f505121f
avinash-123-del <avichandraker@gmail.com>
2023-08-10 19:06:42 -1200

    icon added

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -5,6 +5,8 @@ import { toast } from 'react-hot-toast'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 import Image from 'next/image'
+import { GiHamburgerMenu} from 'react-icons/gi'
+import {GrFormClose} from 'react-icons/gr'
 const Header = () => {
   const [menu,setMenu] = useState(false)
 const router = useRouter()
@@ -30,7 +32,7 @@ const router = useRouter()
 }
 
   return (
-    <div className='shadow-lg py-2 bg-stone-200 w-screen'>
+    <div className='shadow-lg py-2 bg-stone-200 w-screen overflow-x-hidden'>
         <div className='flex items-center justify-between w-[80%] m-auto relative'>
             <Link href='/'>
                 <Image src='/logo.png' width={100} height={50} alt='logo'/>
@@ -44,9 +46,18 @@ const router = useRouter()
 
                 <span className='flex items-center justify-around gap-1 cursor-pointer' onClick={handleLogout}><AiOutlinePoweroff/><span>Logout</span></span>
             </div>
-            <div className='sm:hidden font-bold' onClick={handleMenu}>Menu</div>
 
-            <div onClick={() => setMenu(!menu)} style={menu ? {top:'70px'} : {top:'-400px'} } className={'font-semibold flex sm:hidden absolute flex-col items-center justify-between gap-8 text-xl z-10 transition-all duration-300 right-[-30px] rounded-md py-3 bg-gradient-to-b from-red-950 to-red-900 w-[50%] text-stone-400'}>
+            <div className='sm:hidden font-bold ' onClick={handleMenu} >
+              {
+                menu ?  <GrFormClose size={40} width={30}/> : <GiHamburgerMenu size={30}/>
+              }
+              
+              </div>
+
+            <div onClick={() => setMenu(!menu)} 
+             className={`font-semibold flex sm:hidden fixed flex-col items-start pl-[40px] justify-between gap-8 text-xl transition-all duration-300 right-[-50px] top-[75px] rounded-md py-3 bg-gradient-to-b from-red-950 to-red-900 w-[60%] text-stone-400 z-40
+             ${menu ? 'translate-x-0' : 'translate-x-full'} `}>
+
                 <Link href='/movies/home'><span>Home</span></Link>
                 <Link href='/movies/about'><span>About</span></Link>
                 <Link href='/movies/movie'><span>Movie</span></Link>
--- src/app/layout.js
+++ src/app/layout.js
@@ -13,10 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head><link rel="icon" href="/fav/favicon.ico" /></head>
       <body className={inter.className}>
-
-      
-
         <Toaster/>
         {children}
 
